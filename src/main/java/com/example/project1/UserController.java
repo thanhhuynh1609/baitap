ce4f2d9914bf86d58761f491a0e39783d97872e0
thanhhuynh1609 <hnkt1609@gmail.com>
2025-01-11 13:31:43 +0700

    bao ngu

--- src/main/java/com/example/project1/UserController.java
+++ src/main/java/com/example/project1/UserController.java
@@ -4,17 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.project1.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class UserController {
 
     @GetMapping("/userDetail")
     public String getUserDetail(Model model) {
-        // Hardcode thông tin người dùng
-        model.addAttribute("username", "Le Anh Thanh");
-        model.addAttribute("email", "leanhthanh@gmail.com");
-        model.addAttribute("phone", "0878112334");
-        model.addAttribute("address", "29, Truong Son, Hoa Tho Tay, Cam Le, Da Nang");
+        List<User> users = new ArrayList<>();
+        users.add(new User("Le Anh Thanh", "leanhthanh@gmail.com", "0878112334", "29, Truong Son, Hoa Tho Tay, Cam Le, Da Nang"));
+        users.add(new User("Nguyen Van Binh", "nguyenvanbinh@gmail.com", "0977123456", "15, Nguyen Van Linh, Hai Chau, Da Nang"));
 
-        return "userDetail"; // Trả về file userDetail.html trong thư mục templates
+        model.addAttribute("users", users);
+
+        return "userDetail";
     }
 }
